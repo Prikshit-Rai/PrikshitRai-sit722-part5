1d91b93098f9b630a84d0a2f6498d90bc825b9e9
Prikshit-Rai <56683718+Prikshit-Rai@users.noreply.github.com>
2024-10-06 14:46:08 +1100

    Update resource-group.tf

--- scripts/terraform/resource-group.tf
+++ scripts/terraform/resource-group.tf
@@ -1,7 +1,7 @@
 #
 # Creates a resource group for flixtube in your Azure account.
 #
-resource "azurerm_resource_group" "flixtube" {
+resource "azurerm_resource_group" "task92dprikshit" {
   name     = var.app_name
   location = var.location
 }
